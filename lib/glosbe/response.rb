c7b7f5e0182e73fae70529928efd3f573fa2304e
Kevin McPhillips <github@kevinmcphillips.ca>
2017-09-08 22:35:56 -0400

    Parse out fields from response

--- lib/glosbe/response.rb
+++ lib/glosbe/response.rb
@@ -1,6 +1,34 @@
 # frozen_string_literal: true
 class Glosbe::Response
+  attr_reader :from, :to, :phrase, :sources, :errors
+
   def initialize(http_response)
     @http = http_response
+  end
+
+  def from
+    body["from"]
+  end
+
+  def to
+    body["dest"]
+  end
+
+  def success?
+    body["result"] == "ok"
+  end
+
+  def found?
+    success? && results.any?
+  end
+
+  def results
+    body["tuc"]
+  end
+
+  private
+
+  def body
+    @http.parsed_response
   end
 end
--- spec/glosbe/response_spec.rb
+++ spec/glosbe/response_spec.rb
@@ -2,7 +2,58 @@
 require "spec_helper"
 
 RSpec.describe Glosbe::Response do
-  describe "#initialize" do
+  shared_context "success" do
+    use_vcr_cassette "translate_eng_fr_hello"
 
+    let(:language) { Glosbe::Language.new(from: :eng, to: :fr) }
+    let(:response) { language.response("hello") }
+  end
+
+  describe "#initialize" do
+    it "should be tested"
+  end
+
+  describe "#from" do
+    include_context "success"
+
+    it "parses the from out of the response" do
+      expect(response.from).to eq("en")
+    end
+  end
+
+  describe "#to" do
+    include_context "success"
+
+    it "parses the to out of the response" do
+      expect(response.to).to eq("fr")
+    end
+  end
+
+  describe "#success?" do
+    include_context "success"
+
+    it "looks at the result" do
+      expect(response.success?).to eq(true)
+    end
+  end
+
+  describe "#found?" do
+    include_context "success"
+
+    it "looks at the success and number of results" do
+      expect(response.found?).to eq(true)
+    end
+
+    it "with no results found"
+  end
+
+  describe "#results" do
+    include_context "success"
+
+    it "parses out the results" do
+      expect(response.results).to_not be_empty
+    end
+
+    it "parses the results"
   end
 end
